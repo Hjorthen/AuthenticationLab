720955a08ea836e9a535cbfc0b3aaad747df91e0
Thomas <Thomas@LAPTOP-9VLQN7F2>
2019-10-23 15:58:56 +0200

    Generated interface stub

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,8 +1,64 @@
 package Server;
 
-public class Server {
+import rmi.PrinterInterface;
+
+public class Server implements PrinterInterface {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void print(String filename, String printer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void queue() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void topQueue(int job) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void restart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void status() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void readConfig(String parameter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setConfig(String parameter, String value) {
 		// TODO Auto-generated method stub
 		
 	}
