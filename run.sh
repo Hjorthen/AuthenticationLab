c4a009976deb24ba9e227c1c023e3a09dc40bece
Sebastian Hjorth <Hjorthensebastian@gmail.com>
2019-11-03 12:17:30 +0100

    Added runscript

---
+++ run.sh
@@ -0,0 +1,8 @@
+if ! pgrep -x "gedit" > /dev/null
+then
+    cd bin/
+    rmiregistry &
+    cd ..
+fi
+java -classpath bin/ -Djava.rmi.server.codebase=file:bin/ --module-path mysql-connector-java-8.0.18/ Server.Server &
+java  -classpath bin/ Client.Client
