46cf009dfdca22daf3e036891d6e00755d75bb6b
Sebastian Hjorth <Hjorthensebastian@gmail.com>
2019-11-03 14:59:43 +0100

    Added minimal required information to access token

--- src/Server/AccessToken.java
+++ src/Server/AccessToken.java
@@ -1,8 +1,12 @@
 package Server;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class AccessToken implements Serializable{
+	String receipient;
+	String issuer;
+	Date expiration;
 	
 	public AccessToken() {
 		
