266d8f6359210fe94a55257c5a17bdf4fc811b63
Sebastian Hjorth <Hjorthensebastian@gmail.com>
2019-11-06 16:56:16 +0100

    AuthenticationException to not rely on javax exception type

---
+++ src/Server/AuthenticationException.java
@@ -0,0 +1,9 @@
+package Server;
+
+@SuppressWarnings("serial")
+public class AuthenticationException extends Exception {
+	public AuthenticationException(String message)
+	{
+		super(message);
+	}
+}
