2d1a9cbf4ae2c7d6c63177eaf8b96421a3e7291c
ThomasBechMadsen <thomasbm@live.dk>
2019-11-13 16:02:22 +0100

    Split SQL into two

--- ACL_AccessLab.sql
+++ ACL_AccessLab.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS AuthenticationLab; 
 USE AuthenticationLab;
 
-CREATE TABLE IF NOT EXISTS Role(
-    Title VARCHAR(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS AccessControlMatrix(
+    UserID INT UNSIGNED NOT NULL,
 	Print BOOLEAN DEFAULT false,
     Queue BOOLEAN DEFAULT false,
     TopQueue BOOLEAN DEFAULT false,
@@ -12,7 +12,8 @@ CREATE TABLE IF NOT EXISTS Role(
     Status BOOLEAN DEFAULT false,
     ReadConfig BOOLEAN DEFAULT false,
     SetConfig BOOLEAN DEFAULT false,
-    PRIMARY KEY (Title)
+    FOREIGN KEY (UserID)
+		REFERENCES Account(ID)
 );
 
 CREATE TABLE IF NOT EXISTS Account(
@@ -20,19 +21,18 @@ CREATE TABLE IF NOT EXISTS Account(
     Username VARCHAR(64) NOT NULL,
 	Password VARCHAR(64) NOT NULL, -- Length of SHA-256 hash
 	Salt VARCHAR(32) NOT NULL,
-    Role VARCHAR(20) NOT NULL,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (Role)
-		REFERENCES Role(Title)
+    PRIMARY KEY (ID)
 );
 
 DELIMITER $$
 CREATE PROCEDURE RegisterAccount
-(IN username VARCHAR(64), IN password VARCHAR(64), IN salt VARCHAR(32), IN role VARCHAR(20), OUT id INT)
+(IN username VARCHAR(64), IN password VARCHAR(64), IN salt VARCHAR(32), IN VARCHAR(20), OUT id INT)
 BEGIN
 	INSERT INTO account (Username, Password, Salt, Role)
     VALUES (username, password, salt, role);
-    SET id = LAST_INSERT_ID();
+    
+	SET id = LAST_INSERT_ID();
+	
 END$$
 DELIMITER ;
 
@@ -57,6 +57,16 @@ BEGIN
 END$$
 DELIMITER ;
 
+DELIMITER $$
+CREATE PROCEDURE GetPermissions
+(IN username VARCHAR(64))
+BEGIN
+	SELECT * 
+	FROM AccessControlMatrix
+    WHERE AccessControlMatrix.user = username; 
+END$$
+DELIMITER ;
+
 INSERT INTO role (Title, Print, Queue, TopQueue, Start, Stop, Restart, Status, ReadConfig, SetConfig) VALUES 
 ('Admin', 1, 1, 1, 1, 1, 1, 1, 1, 1),
 ('ServiceTechnician', 0, 0, 0, 1, 1, 1, 1, 1, 1),
@@ -64,7 +74,7 @@ INSERT INTO role (Title, Print, Queue, TopQueue, Start, Stop, Restart, Status, R
 ('User', 1, 1, 0, 0, 0, 0, 0, 0, 0)
 ;
 
-CALL RegisterAccount('Alice', 'test','test','Admin', @result);
+CALL RegisterAccount('Alice', '','', @result);
 CALL RegisterAccount('Bob', '','', @result);
 CALL RegisterAccount('Cecilia', '','', @result);
 CALL RegisterAccount('Bob', '','', @result);
---
+++ RB_AccessLab.sql
@@ -0,0 +1,76 @@
+CREATE DATABASE IF NOT EXISTS AuthenticationLab; 
+USE AuthenticationLab;
+
+CREATE TABLE IF NOT EXISTS Role(
+    Title VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (Title)
+);
+
+CREATE TABLE IF NOT EXISTS RoleRelation(
+	UserID INT UNSIGNED NOT NULL,
+    Role VARCHAR(20) NOT NULL,
+	FOREIGN KEY (UserID)
+		REFERENCES Account(ID),
+	FOREIGN KEY (Role)
+		REFERENCES Role(Title)
+);
+
+CREATE TABLE IF NOT EXISTS Account(
+	ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    Username VARCHAR(64) NOT NULL,
+	Password VARCHAR(64) NOT NULL, -- Length of SHA-256 hash
+	Salt VARCHAR(32) NOT NULL,
+    Role VARCHAR(20) NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (Role)
+		REFERENCES Role(Title)
+);
+
+DELIMITER $$
+CREATE PROCEDURE RegisterAccount
+(IN username VARCHAR(64), IN password VARCHAR(64), IN salt VARCHAR(32), IN role VARCHAR(20), OUT id INT)
+BEGIN
+	INSERT INTO account (Username, Password, Salt, Role)
+    VALUES (username, password, salt, role);
+    SET id = LAST_INSERT_ID();
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE LookupSalt
+(IN username VARCHAR(64))
+BEGIN 
+	select Account.salt from account where account.username = username;
+END$$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE PROCEDURE AuthenticateUser
+(IN username VARCHAR(64), IN password VARCHAR(64))
+BEGIN
+	SELECT EXISTS(
+		SELECT account.id
+		FROM account
+		WHERE account.username = username AND account.password = password
+    );
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE GetRole
+(IN username VARCHAR(64))
+BEGIN
+	
+END$$
+DELIMITER ;
+
+CALL RegisterAccount('Alice', '','','Admin', @result);
+CALL RegisterAccount('Bob', '','','ServiceTechnician', @result);
+CALL RegisterAccount('Cecilia', '','','SuperUser', @result);
+CALL RegisterAccount('Bob', '','','User', @result);
+CALL RegisterAccount('David', '','','User', @result);
+CALL RegisterAccount('Erica', '','','User', @result);
+CALL RegisterAccount('Fred', '','','User', @result);
+CALL RegisterAccount('George', '','','User', @result);
